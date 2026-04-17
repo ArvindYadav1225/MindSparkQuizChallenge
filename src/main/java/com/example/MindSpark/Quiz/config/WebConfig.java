b01810d4d3c95d83951f4a630bebee2b76497526
ArvindYadav1225 <9369874330arvind@gmail.com>
2026-04-17 10:29:55 +0530

    config folder with two files AuthInterceptor.java and AuthInterceptor.java is added

---
+++ src/main/java/com/example/MindSpark/Quiz/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.MindSpark.Quiz.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/signup",
+                        "/login",
+                        "/submitLogin",
+                        "/submitSignup",
+                        "/sendOtp/**",
+                        "/otpVerification/**",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**"
+                );
+    }
+}
